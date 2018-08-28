9bb1dbcdaa05352957d7171d0d15a12a15d4b96b
TzuYuYang <queen64528@gmai.com>
2018-08-28 23:09:10 +0800

    the second version

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
-i = 1
-while i < 4 :
+i = 3
+while i > 0 :
 	password = input('請輸入密碼：')
 	if password == 'a123456':
 		print ('登入成功')
 		break
-	else :
-		print ('密碼錯誤！還有', 3-i , '次機會')
-		i = i + 1
+	else: 
+		print ('密碼錯誤！')
+		i = i - 1
+		if i > 0:
+			print ('還有', i , '次機會')
+		else:
+			break
 
